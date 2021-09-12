7a41b4843fb49b76e8acd1492d67a3ae1521f19c
tagarwal-NCSU <tagarwa@ncsu.edu>
2021-09-12 17:24:37 -0400

    Updated ESPNAPI.py to display position names instead of IDs

--- Taylor Agarwal/ESPNAPI.py
+++ Taylor Agarwal/ESPNAPI.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[12]:
 
 
 import pprint
 import requests
 import pandas as pd
 from ESPNIDs import POSITION_MAP
+import json
 
 class League:
     
@@ -17,14 +18,14 @@ class League:
         self.espn_s2 = espn_s2
         self.swid = swid
 
-    def fetch(self, view):
+    def fetch_raw(self, view, headers=None):
         URL = "https://fantasy.espn.com/apis/v3/games/ffl/seasons/" + self.year + "/segments/0/leagues/" + self.leagueID + "?view=" + view
-        r = requests.get(URL, cookies={"espn_s2": self.espn_s2, "swid": self.swid})
+        r = requests.get(URL, cookies={"espn_s2": self.espn_s2, "swid": self.swid}, headers=headers)
         data = r.json()
         return data
 
     def fetch_teams(self):
-        team_data = self.fetch("mTeam")
+        team_data = self.fetch_raw("mTeam")
         Teams = dict()
         members = team_data['members']
         member_lookup = dict()
@@ -44,32 +45,57 @@ class League:
                               'trades': team['transactionCounter']['trades']
                               }
             team_id += 1
-        #return self.fetch("mTeam")
-        return Teams
+        return self.fetch("mTeam")
+        #return Teams
     
     def fetch_rosters(self):
-        roster_data = self.fetch("mRoster")
+        roster_data = self.fetch_raw("mRoster")
         Rosters = dict()
         teams = roster_data['teams']
         for team in teams:
             team_id = team['id']
             roster_entries = team['roster']['entries']
-            Rosters[team_id] = {'players': [entry['playerPoolEntry']['player']['fullName'] for entry in roster_entries],
-                                'positions': [entry['playerPoolEntry']['player']['defaultPositionId'] for entry in roster_entries]}
+            Rosters[team_id] = {'roster': [entry['playerPoolEntry']['player']['fullName'] for entry in roster_entries],
+                                'defaultPosition': [entry['playerPoolEntry']['player']['defaultPositionId'] for entry in roster_entries]}
+            Rosters[team_id]['defaultPosition'] = [POSITION_MAP[pos] for pos in Rosters[team_id]['defaultPosition']]
         return Rosters
     
     def fetch_draft(self):
         #TODO
-        return self.fetch("mDraftDetail")
+        return self.fetch_raw("mDraftDetail")
     
-    def fetch_players(self):
-        return self.fetch("mMatchup&scoringPeriodId=1")
+    def fetch_free_agents(self):
+        #TODO
+        filters = {"players":{"filterStatus":{"value":["FREEAGENT","WAIVERS"]}, 
+                              "limit":50,
+                              "sortPercOwned":{"sortPriority":1,
+                                               "sortAsc":False},
+                              "sortDraftRanks":{"sortPriority":100,
+                                                "sortAsc":True,
+                                                "value":"STANDARD"}
+                             }
+                  }
+        headers = {'x-fantasy-filter': json.dumps(filters)}
+        Free_Agents = dict()
+        free_agent_data = self.fetch_raw("kona_player_info&scoringPeriodId="+str(i), headers)
+        for i, player in enumerate(free_agent_data['players']):
+            Free_Agents[j+1] = {'fullName': player['player']['fullName'],
+                                       'injured': player['player']['injured'],
+                                       'percentOwned': player['player']['ownership']['percentOwned'],
+                                       'eligibleSlots': player['player']['eligibleSlots']
+                                      }
+            Free_Agents[j+1]['eligibleSlots'] = [POSITION_MAP[pos] for pos in Free_Agents[j+1]['eligibleSlots']]
+        
+        return Free_Agents
+        #return self.fetch_raw("kona_player_info", headers)
     
     def fetch_matchup_rosters(self):
         Matchups = dict()
         for i in range(1, 20):
-            matchup_data = self.fetch("mMatchup&scoringPeriodId=" + str(i))
+            matchup_data = self.fetch_raw("mMatchup&scoringPeriodId=" + str(i))
             matchups = matchup_data['schedule']
+            #reader = pprint.PrettyPrinter()
+            #reader.pprint(matchups)
             for matchup in matchups:
                 matchup_id = matchup['id']
                 if 'rosterForCurrentScoringPeriod' not in matchup['home'].keys():
@@ -79,19 +105,24 @@ class League:
                                         'homeScore': matchup['home']['totalPoints'],
                                         'homeRoster': [entry['playerPoolEntry']['player']['fullName'] for entry in matchup['home']['rosterForCurrentScoringPeriod']['entries']],
                                         'homeLineup': [entry['lineupSlotId'] for entry in matchup['home']['rosterForCurrentScoringPeriod']['entries']],
-                                        'homeRosterScore': [entry['playerPoolEntry']['appliedStatTotal'] for entry in matchup['home']['rosterForCurrentScoringPeriod']['entries']],
+                                        'homeRosterScore': [round(entry['playerPoolEntry']['appliedStatTotal'], 2) for entry in matchup['home']['rosterForCurrentScoringPeriod']['entries']],
                                         'homeRosterEligibleSlots': [entry['playerPoolEntry']['player']['eligibleSlots'] for entry in matchup['home']['rosterForCurrentScoringPeriod']['entries']],
                                         'awayTeamId': matchup['away']['teamId'],
                                         'awayScore': matchup['away']['totalPoints'],
                                         'awayRoster': [entry['playerPoolEntry']['player']['fullName'] for entry in matchup['away']['rosterForCurrentScoringPeriod']['entries']],
                                         'awayLineup': [entry['lineupSlotId'] for entry in matchup['away']['rosterForCurrentScoringPeriod']['entries']],
-                                        'awayRosterScore': [entry['playerPoolEntry']['appliedStatTotal'] for entry in matchup['away']['rosterForCurrentScoringPeriod']['entries']],
+                                        'awayRosterScore': [round(entry['playerPoolEntry']['appliedStatTotal'],2) for entry in matchup['away']['rosterForCurrentScoringPeriod']['entries']],
                                         'awayRosterEligibleSlots': [entry['playerPoolEntry']['player']['eligibleSlots'] for entry in matchup['away']['rosterForCurrentScoringPeriod']['entries']],
-                                        'winner': matchup['home']['teamId'] if matchup['winner'] == "HOME" else matchup['away']['teamId']}
+                                        'winner': matchup['home']['teamId'] if matchup['winner'] == "HOME" else matchup['away']['teamId']
+                                       }
+                Matchups[matchup_id]['homeLineup'] = [POSITION_MAP[pos] for pos in Matchups[matchup_id]['homeLineup']]
+                Matchups[matchup_id]['homeRosterEligibleSlots'] = [[POSITION_MAP[pos] for pos in lst] for lst in Matchups[matchup_id]['homeRosterEligibleSlots']]
+                Matchups[matchup_id]['awayLineup'] = [POSITION_MAP[pos] for pos in Matchups[matchup_id]['awayLineup']]
+                Matchups[matchup_id]['awayRosterEligibleSlots'] = [[POSITION_MAP[pos] for pos in lst] for lst in Matchups[matchup_id]['awayRosterEligibleSlots']]
         return Matchups
     
     def fetch_matchups(self):
-        matchup_data = self.fetch("mMatchup")
+        matchup_data = self.fetch_raw("mMatchup")
         Matchups = dict()
         matchups = matchup_data['schedule']
         for matchup in matchups:
@@ -103,10 +134,4 @@ class League:
                                     'awayScore': matchup['away']['totalPoints'],
                                     'winner': matchup['home']['teamId'] if matchup['winner'] == "HOME" else matchup['away']['teamId']}
         return Matchups
-
-
-# In[ ]:
-
-
-
 
